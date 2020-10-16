8dc20b423fffd150e9eced85293b509454099968
Anson <anson.chung@gmail.com>
2020-10-16 11:58:50 -0400

    first commit

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,5 @@
+const githubClientId = '4caf37b2ca0a77c91188';
+const githubClientSecret = 'a65256171ae25e9525411bd0f46219a8b433bf3b';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
